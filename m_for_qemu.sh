9a7de8813d2689cc5d2e6e77d5aea8864f756ab1
winddoing <winddoing@sina.com>
2018-12-13 15:38:53 +0800

    add coustom configure

---
+++ m_for_qemu.sh
@@ -0,0 +1,11 @@
+##########################################################
+# File Name		: m_for_qemu.sh
+# Author		: winddoing
+# Created Time	: 2018年12月10日 星期一 15时02分49秒
+# Description	:
+##########################################################
+#!/bin/bash
+
+make qemu_arm_vexpress_a_defconfig
+
+make
