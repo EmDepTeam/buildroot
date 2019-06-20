a52159500c2c39d13a8d630a8ff02ee8863d63fa
winddoing <winddoing@sina.cn>
2019-06-20 17:01:13 +0800

    add aarch64 configure

--- m_for_qemu.sh
+++ m_for_qemu.sh
@@ -5,7 +5,21 @@
 # Description	:
 ##########################################################
 #!/bin/bash
+if [ ! -n "${TOPDIR}" ];then
+	echo "Please execute the command (source envsetup.sh) in the project root directory"
+	exit 1;
+fi
 
-make qemu_arm_vexpress_a_defconfig
+ARCH=arm64
+IMAGE="${TOPDIR}/image_file"
 
-make
+if [ $ARCH == "arm64" ]; then
+	echo "Build arm64 ..."
+	#make qemu_aarch64_virt_defconfig
+	make qemu_aarch64_virt_a_defconfig
+	make
+else
+	echo "Build arm ..."
+	make qemu_arm_vexpress_a_defconfig
+	make
+fi
